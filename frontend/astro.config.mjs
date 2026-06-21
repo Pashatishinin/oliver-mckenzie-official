e824e07e71ccb30341286cd08494fc3bb11c8b09
Pavlo Tishynin <pashatishinin@gmail.com>
2026-06-21 14:26:57 +0200

    studio

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,28 +1,16 @@
 // @ts-check
 
-import react from "@astrojs/react";
-import sanity from "@sanity/astro";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import { loadEnv } from "vite";
-
-const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
-	process.env.NODE_ENV ?? "development",
-	process.cwd(),
-	"",
-);
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		react(),
-		sanity({
-			projectId: PUBLIC_SANITY_PROJECT_ID,
-			dataset: PUBLIC_SANITY_DATASET,
-			useCdn: false,
-			studioBasePath: "/studio",
-		}),
-	],
+	redirects: {
+		"/studio": {
+			status: 301,
+			destination: "https://oliver-mckenzie.sanity.studio/",
+		},
+	},
 	vite: {
 		plugins: [tailwindcss()],
 	},
